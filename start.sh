3e0e1acb83742ede827fd20c12257a556a5e2ee8
Wers <112427364+w9line@users.noreply.github.com>
2026-03-29 21:18:43 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./client -server="$SERVER" -session-id="$SESSION_ID" -mode="$MODE" -log="$LOG" &
-CLIENT_PID=$!
-
-echo "Client PID: $CLIENT_PID"
-
-exec gost -L "socks5+ws://${GOST_USER}:${GOST_PASS}@:${GOST_PORT}"
